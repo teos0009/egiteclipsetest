4706b0c57585994fdefa2e05069ae366add714ac
shin <shin@ultraman7>
2013-06-17 17:27:43 +0100

    test add new java class andra.java

---
+++ src/andra.java
@@ -0,0 +1,17 @@
+
+public class andra {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    profFile("thepath");
+	}//end main
+	
+	private static void profFile(String string) {
+		// TODO Auto-generated method stub
+		System.out.println("thePath");
+	}
+
+}
